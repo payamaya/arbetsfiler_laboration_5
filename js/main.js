7e84d0f45636077da6a136e5f14bc8319bdee949
payamaya <paulyashou81@gmail.com>
2026-09-25 20:45:34 +0200

    fix: validate the required fields, log errors and form submission flow

--- js/main.js
+++ js/main.js
@@ -1,117 +1,126 @@
-"use strict";
+'use strict'
 /*
  * Laboration 5 - Studentkortsgenerator
- * Namn: DITT NAMN
+ * Namn: Paul Yashouh
  */
 
 // Hämta element från DOM
-const form = document.querySelector("#studentform");
-const clearButton = document.querySelector("#clear");
+const form = document.querySelector('#studentform')
+const clearButton = document.querySelector('#clear')
 
-const fullnameInput = document.querySelector("#fullname");
-const emailInput = document.querySelector("#email");
-const phoneInput = document.querySelector("#phone");
-const fontSelect = document.querySelector("#font");
+const fullnameInput = document.querySelector('#fullname')
+const emailInput = document.querySelector('#email')
+const phoneInput = document.querySelector('#phone')
+const fontSelect = document.querySelector('#font')
 
-const previewFullname = document.querySelector("#previewfullname");
-const previewEmail = document.querySelector("#previewemail");
-const previewPhone = document.querySelector("#previewphone");
+const previewFullname = document.querySelector('#previewfullname')
+const previewEmail = document.querySelector('#previewemail')
+const previewPhone = document.querySelector('#previewphone')
 
-const errorList = document.querySelector("#errorlist");
-const historySection = document.querySelector("#history");
-const deleteHistoryButton = document.querySelector("#delete");
-
+const errorList = document.querySelector('#errorlist')
+const historySection = document.querySelector('#history')
+const deleteHistoryButton = document.querySelector('#delete')
 
 // Array som används för felmeddelanden
-let errors = [];
+let errors = []
 
 // Array som innehåller sparade studentkort
-let history = [];
+let history = []
 
 /**
  * Validerar formulärets inmatning.
  * @returns {boolean}
  */
 function validateForm() {
+  form.addEventListener('submit', (e) => {
     // Kontrollera formulärets obligatoriska fält
+    const name = fullnameInput.value.trim()
+    const email = emailInput.value.trim()
+    const phone = phoneInput.value.trim()
 
+    let errors = ''
     // Visa eventuella felmeddelanden
+    if (name === '') {
+      errors = 'Måste ange fullständiga namn'
+      console.log('Måste ange fullständiga namn')
+    } else if (email === '') {
+      errors = 'Måste ange fullständiga e-post address'
+      console.log('Måste ange fullständiga e-post address')
+    } else if (phone === '') {
+      errors = 'Måste ange fullständiga phone nummer'
+      console.log('Måste ange fullständiga phone nummer')
+    }
+
+    if (errors !== '') {
+      e.preventDefault()
+      console.log('errors : ', errors)
+      return
+    }
 
     // Returnera resultatet (true eller false) av valideringen
+    console.log('Data sent')
+    return true
+  })
 }
 
+validateForm()
 
 /**
  * Visar felmeddelanden på sidan.
  */
 function displayErrors() {
-    // Rensa tidigare felmeddelanden
-
-    // Skriv ut aktuella felmeddelanden till DOM
+  // Rensa tidigare felmeddelanden
+  // Skriv ut aktuella felmeddelanden till DOM
 }
-
 
 /**
  * Skapar ett studentkort och visar det på sidan.
  */
 function createStudentCard() {
-    // Hämta information från formuläret
-
-    // Uppdatera studentkortet
-
-    // Lägg till studentkortet i historiken
-
-    // Spara och uppdatera historiken
+  // Hämta information från formuläret
+  // Uppdatera studentkortet
+  // Lägg till studentkortet i historiken
+  // Spara och uppdatera historiken
 }
-
 
 /**
  * Sparar historiken i localStorage.
  */
 function saveHistory() {
-    // Spara history i localStorage
+  // Spara history i localStorage
 }
-
 
 /**
  * Läser in tidigare historik från localStorage.
  */
 function loadHistory() {
-    // Hämta eventuell sparad historik
-
-    // Uppdatera history
+  // Hämta eventuell sparad historik
+  // Uppdatera history
 }
-
 
 /**
  * Visar historiken på sidan.
  */
 function renderHistory() {
-    // Rensa tidigare visad historik
-
-    // Skriv ut innehållet i history till DOM
+  // Rensa tidigare visad historik
+  // Skriv ut innehållet i history till DOM
 }
-
 
 /**
  * Rensar formulär, aktuellt studentkort och felmeddelanden.
  */
 function clearForm() {
-    // Återställ formulär och studentkort
-
-    // Rensa eventuella felmeddelanden
+  // Återställ formulär och studentkort
+  // Rensa eventuella felmeddelanden
 }
-
 
 /**
  * Raderar hela historiken.
  */
 function deleteHistory() {
-    // Radera sparad historik
-
-    // Uppdatera history och visningen på sidan
+  // Radera sparad historik
+  // Uppdatera history och visningen på sidan
 }
-
 
 // Eventlyssnare
 
@@ -119,12 +128,9 @@ function deleteHistory() {
 // - validera inmatningen
 // - skapa studentkort om valideringen lyckas
 
-
 // När användaren klickar på "Rensa"
 
-
 // När användaren klickar på "Radera historik"
-
 
 // När sidan laddas:
 // - läs in och visa eventuell tidigare historik
